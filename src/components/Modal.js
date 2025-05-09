88d11cbb6e5ef022be7c423425bf76245a0f9599
Nowshin Inside Job <126184176+NowshinRahman@users.noreply.github.com>
2025-05-09 12:14:34 -0400

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,1 +1,15 @@
+import React from "react";
+
+const Modal = ({ message, onClose }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <h2>{message}</h2>
+        <button onClick={onClose}>Okay</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
 
